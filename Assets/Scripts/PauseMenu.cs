73294e81e368661e30a0e9e1bf09d2bdfd1fe434
Vincent <vincentboutin.dev@gmail.com>
2023-09-05 14:09:36 +0200

    Create Pause Menu

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+
+    [SerializeField] private KeyCode keyPause;
+    [SerializeField] 
+    private GameObject menuParent;
+    [SerializeField] 
+    private Button resumeButton, menuButton;
+
+    private bool isPaused;
+
+    private void Start()
+    {
+        resumeButton.onClick.AddListener(Resume);
+        menuButton.onClick.AddListener(BackToMenu);
+    }
+
+    private void OnDestroy()
+    {
+        resumeButton.onClick.RemoveListener(Resume);
+        menuButton.onClick.RemoveListener(BackToMenu);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(keyPause))
+        {
+            if (isPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    private void Pause()
+    {
+        isPaused = true;
+        menuParent.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    private void Resume()
+    {
+        isPaused = false;
+        menuParent.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    private void BackToMenu()
+    {
+        Resume();
+        SceneManager.LoadScene(0);
+    }
+}
