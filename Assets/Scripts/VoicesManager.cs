fea23697c7020d6a3c17af0017998b1067e03ad9
Vincent <vincentboutin.dev@gmail.com>
2023-09-08 10:04:21 +0200

    Add voices with input

---
+++ Assets/Scripts/VoicesManager.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class VoicesManager : MonoBehaviour
+{
+    [SerializeField]
+    private List<AudioClip> _clips;
+    private List<AudioSource> _sources;
+    private bool _isPlaying;
+
+    private void Start()
+    {
+        _sources = new List<AudioSource>();
+        foreach (AudioClip clip in _clips)
+        {
+            clip.LoadAudioData();
+            AudioSource source = transform.AddComponent<AudioSource>();
+            source.playOnAwake = false;
+            source.clip = clip;
+            _sources.Add(source);
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(InputManager.Instance.GetKeyCodeFromInput(MachineInput.ButtonWhite)))
+        {
+            PlayRandomVoice();
+        }
+    }
+
+    private void PlayRandomVoice()
+    {
+        if (_isPlaying)
+            return;
+        _isPlaying = true;
+        int rand = Random.Range(0, _sources.Count);
+        _sources[rand].Play();
+        StartCoroutine(Wait(_sources[rand].clip.length));
+
+    }
+
+    private IEnumerator Wait(float time)
+    {
+        yield return new WaitForSeconds(time);
+        _isPlaying = false;
+    }
+}
